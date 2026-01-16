e70a68624360a40d8298aface6e485f9122094c2
Shikha <shiikhaa938@gmail.com>
2026-01-17 00:36:20 +0530

    Time: 44 ms (34.79%), Space: 46.5 MB (81.67%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,33 +1,15 @@
-import java.util.HashMap;
-
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> map = new HashMap<>(); // value -> index
-
         for (int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i];
+            for (int j = i + 1; j < nums.length; j++) {
 
-            if (map.containsKey(complement)) {
-                return new int[] { map.get(complement), i };
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+
             }
-
-            map.put(nums[i], i);
         }
-
-        return new int[] {}; // fallback, though question says one solution exists
-    }
-
-    // main method for testing
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-
-        int[] result1 = solution.twoSum(new int[]{2, 7, 11, 15}, 9);
-        System.out.println("Output: [" + result1[0] + ", " + result1[1] + "]");
-
-        int[] result2 = solution.twoSum(new int[]{3, 2, 4}, 6);
-        System.out.println("Output: [" + result2[0] + ", " + result2[1] + "]");
-
-        int[] result3 = solution.twoSum(new int[]{3, 3}, 6);
-        System.out.println("Output: [" + result3[0] + ", " + result3[1] + "]");
+        return new int[]{};
+    
     }
 }
