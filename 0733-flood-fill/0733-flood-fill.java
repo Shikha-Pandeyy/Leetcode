3a68a3eb8e6f78d3ded4c4548c2f21094f7c5846
Shikha <shiikhaa938@gmail.com>
2026-03-22 23:11:13 +0530

    Time: 0 ms (100%), Space: 46.7 MB (87.64%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        
+        int originalColor = image[sr][sc];
+        
+        if (originalColor == color) return image;
+        
+        dfs(image, sr, sc, originalColor, color);
+        return image;
+    }
+
+    private void dfs(int[][] image, int i, int j, int originalColor, int newColor) {
+        if (i < 0 || j < 0 || i >= image.length || j >= image[0].length)
+            return;
+        
+        if (image[i][j] != originalColor)
+            return;
+        image[i][j] = newColor;
+
+        dfs(image, i + 1, j, originalColor, newColor); 
+        dfs(image, i - 1, j, originalColor, newColor); 
+        dfs(image, i, j + 1, originalColor, newColor); 
+        dfs(image, i, j - 1, originalColor, newColor); 
+    }
+}
